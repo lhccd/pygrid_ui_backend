15ce618e34b3c15b721b57581ba199a33ab33c67
Karolina <k.tomaszewska.97@gmail.com>
2021-12-12 18:03:49 +0100

    Changed roles schema

--- backend/app/app/schemas/roles.py
+++ backend/app/app/schemas/roles.py
@@ -1,68 +1,30 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
-class Role(BaseModel):
-    name: Optional[str]
-    can_make_data_requests: Optional[bool] = False
-    can_triage_data_requests: Optional[bool] = False
-    can_manage_privacy_budget: Optional[bool] = False
-    can_create_users: Optional[bool] = False
-    can_manage_users: Optional[bool] = False
-    can_edit_roles: Optional[bool] = False
-    can_manage_infrastructure: Optional[bool] = False
-    can_upload_data: Optional[bool] = False
-    can_upload_legal_document: Optional[bool] = False
-    can_edit_domain_settings: Optional[bool] = False
-
-class RoleAdmin(BaseModel):
-    name: Optional[str] = "Admin"
-    can_make_data_requests: Optional[bool] = True
-    can_triage_data_requests: Optional[bool] = True
-    can_manage_privacy_budget: Optional[bool] = True
-    can_create_users: Optional[bool] = True
-    can_manage_users: Optional[bool] = True
-    can_edit_roles: Optional[bool] = True
-    can_manage_infrastructure: Optional[bool] = False
-    can_upload_data: Optional[bool] = True
-    can_upload_legal_document: Optional[bool] = True
-    can_edit_domain_settings: Optional[bool] = True
 
 class RoleDataScientist(BaseModel):
-    name: Optional[str] = "Data Scientist"
-    can_make_data_requests: Optional[bool] = True
-    can_triage_data_requests: Optional[bool] = False
-    can_manage_privacy_budget: Optional[bool] = False
-    can_create_users: Optional[bool] = False
-    can_manage_users: Optional[bool] = False
-    can_edit_roles: Optional[bool] = False
-    can_manage_infrastructure: Optional[bool] = False
-    can_upload_data: Optional[bool] = False
-    can_upload_legal_document: Optional[bool] = False
-    can_edit_domain_settings: Optional[bool] = False
+    name = "Data Scientist"
+    can_make_data_requests = True
 
-class RoleComplianceOfficer(BaseModel):
-    name: Optional[str] = "Compliance Officer"
-    can_make_data_requests: Optional[bool] = False
-    can_triage_data_requests: Optional[bool] = True
-    can_manage_privacy_budget: Optional[bool] = True
-    can_create_users: Optional[bool] = False
-    can_manage_users: Optional[bool] = True
-    can_edit_roles: Optional[bool] = False
-    can_manage_infrastructure: Optional[bool] = False
-    can_upload_data: Optional[bool] = False
-    can_upload_legal_document: Optional[bool] = False
-    can_edit_domain_settings: Optional[bool] = False
+class RoleAdmin(RoleDataScientist):
+    name = "Admin"
+    can_triage_data_requests = True
+    can_manage_privacy_budget = True
+    can_create_users = True
+    can_manage_users = True
+    can_edit_roles = True
+    can_upload_data = True
+    can_upload_legal_document = True
+    can_edit_domain_settings = True
 
-class RoleOwner(BaseModel):
-    name: Optional[str] = "Owner"
-    can_make_data_requests: Optional[bool] = True
-    can_triage_data_requests: Optional[bool] = True
-    can_manage_privacy_budget: Optional[bool] = True
-    can_create_users: Optional[bool] = True
-    can_manage_users: Optional[bool] = True
-    can_edit_roles: Optional[bool] = True
-    can_manage_infrastructure: Optional[bool] = True
-    can_upload_data: Optional[bool] = True
-    can_upload_legal_document: Optional[bool] = True
-    can_edit_domain_settings: Optional[bool] = True
+class RoleComplianceOfficer(RoleDataScientist):
+    name = "Compliance Officer"
+    can_make_data_requests = False
+    can_triage_data_requests = True
+    can_manage_privacy_budget = True
+    can_manage_users = True
+
+class RoleOwner(RoleAdmin):
+    name = "Owner"
+    can_manage_infrastructure = True
